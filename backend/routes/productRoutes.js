212cf2909bab7c4342997c7f1a63f1defe8ddaba
Mohammad Rezaii <rezaiimohammad003@gmail.com>
2023-02-04 19:56:45 +0330

    user login endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productControllers.js";
 
-// @desc    Fetch all products
-// @api     GET /api/products
-// @access  Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
-// @desc    Fetch single product
-// @api     GET /api/products/:id
-// @access  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,19 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 // Routes
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-
 connectDB();
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 // Error handling
 app.use(notFound);
